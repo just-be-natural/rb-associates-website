28996e58f7199583bc6b75c17c2c5b09c9add6d1
Frank Hock <frank.hock@gmail.com>
2016-08-31 20:11:06 -0400

    wip photo gallery

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,16 @@
 //= require jquery/dist/jquery.min
 //= require jquery.mb.ytplayer/dist/jquery.mb.YTPlayer
 //= require bootstrap
+//= require owl.carousel/dist/owl.carousel.min
 
 //= require 'app'
 
 $(document).ready(function() {
   $('.player').mb_YTPlayer();
+
+  $('.owl-carousel').owlCarousel({
+    items: 3,
+    margin: 15,
+    dots: true
+  });
 });
